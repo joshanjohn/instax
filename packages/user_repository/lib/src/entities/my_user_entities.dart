5a05575df472630b6840c825addd4501b94ca670
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-07-17 15:07:29 +0100

    added my user getter and to, from entiry methods

--- packages/user_repository/lib/src/entities/my_user_entities.dart
+++ packages/user_repository/lib/src/entities/my_user_entities.dart
@@ -1,3 +1,19 @@
-class MyUserEntities{
+import 'package:equatable/equatable.dart';
 
+class MyUserEntities extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  MyUserEntities({
+    required this.id,
+    required this.email,
+    required this.name,
+    this.picture,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [id, email, name, picture];
 }
--- packages/user_repository/lib/src/models/my_user.dart
+++ packages/user_repository/lib/src/models/my_user.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:user_repository/src/entities/entities.dart';
 
 class MyUser extends Equatable {
   final String id;
@@ -6,6 +7,7 @@ class MyUser extends Equatable {
   final String name;
   final String? picture;
 
+  // constructor for MyUser
   const MyUser({
     required this.id,
     required this.email,
@@ -20,7 +22,46 @@ class MyUser extends Equatable {
     picture: '',
   );
 
+  // Modify MyUser parameters
+  MyUser copyWith(
+    String? id,
+    String? email,
+    String? name,
+    String? picture,
+  ) {
+    return MyUser(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      picture: picture ?? this.picture,
+    );
+  }
+
+  // getter method to return if the current user is empty
+  bool get isEmpty => this == MyUser.empty;
+
+  // getter method to return is the current user is not empty
+  bool get isNotEmpty => this == MyUser.empty;
+
+  MyUserEntities toEntity() {
+    return MyUserEntities(
+      id: id,
+      email: email,
+      name: name,
+      picture: picture,
+    );
+  }
+
+  static MyUser fromEntity(MyUserEntities entity) {
+    return MyUser(
+      id: entity.id,
+      email: entity.email,
+      name: entity.name,
+      picture: entity.picture,
+    );
+  }
+
   @override
   // TODO: implement props
-  List<Object?> get props => [];
+  List<Object?> get props => [id, email, name, picture];
 }
