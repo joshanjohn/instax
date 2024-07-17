a39f9a7be4161cf68511558278e8f41b626034ce
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-07-17 14:35:48 +0100

    Added defined user attributes in my_user under models

--- packages/user_repository/lib/src/models/my_user.dart
+++ packages/user_repository/lib/src/models/my_user.dart
@@ -1,3 +1,21 @@
-class MyUser{
+import 'package:equatable/equatable.dart';
+
+class MyUser extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  const MyUser({
+    required this.id,
+    required this.email,
+    required this.name,
+    this.picture,
+  });
+
   
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
