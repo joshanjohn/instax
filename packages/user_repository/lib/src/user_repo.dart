9871334b8236860026ef525c954224aa013c99a2
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-07-18 20:49:40 +0100

    Create abstract methods  signIn, logOut, signUp, resetPassword in abstarct UserRepository class

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,3 +1,11 @@
 abstract class UserRepository{
   
+  Future<void> signIn (String email, String password);
+
+  Future<void> logOut();
+
+  Future<void> signUp(String email, String password);
+
+  Future<void> resetPassword(String email);
+
 }
