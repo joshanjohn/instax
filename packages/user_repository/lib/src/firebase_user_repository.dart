e90ad759893c47857fe4e87877609257b7b4759d
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-07-18 20:53:04 +0100

    Implemented abstract methods in firebase user repository class
    all the implementation throws an unImplemented Error as there is no logic in each methods

--- packages/user_repository/lib/src/firebase_user_repository.dart
+++ packages/user_repository/lib/src/firebase_user_repository.dart
@@ -2,10 +2,30 @@ import 'package:user_repository/src/user_repo.dart';
 
 class FirebaseUserRepository implements UserRepository {
   // sign in
+  @override
+  Future<void> signIn(String email, String password) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
 
   //sign out
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
 
   // sign up
+  @override
+  Future<void> signUp(String email, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
 
   // reset password
+  @override
+  Future<void> resetPassword(String email) {
+    // TODO: implement resetPassword
+    throw UnimplementedError();
+  }
 }
