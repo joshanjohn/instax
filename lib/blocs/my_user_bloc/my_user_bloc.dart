2f3bdcb034e2936df29eec14af09c817a73bcf84
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-07-24 16:29:29 +0100

    Define the success, loading, and failure my user bloc states

--- lib/blocs/my_user_bloc/my_user_bloc.dart
+++ lib/blocs/my_user_bloc/my_user_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
+import 'package:user_repository/user_repository.dart';
 
 part 'my_user_event.dart';
 part 'my_user_state.dart';
